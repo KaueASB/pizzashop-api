712eee65fa86323accfa464646fffb880044114f
Kaue Alves <kaue_alves00@yahoo.com>
2024-02-21 00:20:41 -0300

    feat: setup initial

---
+++ src/http/server.ts
@@ -0,0 +1,8 @@
+import { Elysia } from 'elysia'
+
+const app = new Elysia()
+  .get('/', () => {
+    return 'Hello World'
+  })
+
+app.listen(3333, () => console.log('🚀 HTTP server running: http://localhost:3333'))
