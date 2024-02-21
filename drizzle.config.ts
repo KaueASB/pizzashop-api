305da0455787a66436d110f16210e03ccd36a4ca
Kaue Alves <kaue_alves00@yahoo.com>
2024-02-21 10:36:36 -0300

    feat: Environment variables on Bun

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,8 +1,11 @@
 import type { Config } from "drizzle-kit"
+import { env } from "./src/env";
 
 export default {
   schema: "./src/db/schema/index.ts",
   out: "./drizzle",
   driver: 'pg',
-  dbCredentials: { connectionString: 'postgresql://docker:docker@localhost:5432/pizzashop'}
+  dbCredentials: {
+    connectionString: env.DATABASE_URL
+  }
 } satisfies Config;
