4d49dd959576930a6340ad1502665382bb9e3093
Kaue Alves <kaue_alves00@yahoo.com>
2024-02-21 10:24:46 -0300

    feat: Migrate db - create migrations on db

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -4,4 +4,5 @@ export default {
   schema: "./src/db/schema/index.ts",
   out: "./drizzle",
   driver: 'pg',
+  dbCredentials: { connectionString: 'postgresql://docker:docker@localhost:5432/pizzashop'}
 } satisfies Config;
