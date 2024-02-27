8edca68a07aeb32a60b0451e84edcf8302e31b09
Kaue Alves <kaue_alves00@yahoo.com>
2024-02-27 12:52:45 -0300

    feat: Config - JWT

--- src/http/server.ts
+++ src/http/server.ts
@@ -1,9 +1,24 @@
-import { Elysia } from 'elysia'
+import { Elysia, t } from 'elysia'
 
 import { RegisterRestaurant } from './routes/register-restaurant'
 import { sendAuthLink } from './routes/send-auth-link'
+import jwt from '@elysiajs/jwt'
+import { env } from '../env'
+import cookie from '@elysiajs/cookie'
 
-const app = new Elysia().use(RegisterRestaurant).use(sendAuthLink)
+const app = new Elysia()
+  .use(
+    jwt({
+      secret: env.JWT_SECRET,
+      schema: t.Object({
+        sub: t.String(),
+        restaurantId: t.Optional(t.String()),
+      }),
+    }),
+  )
+  .use(cookie())
+  .use(RegisterRestaurant)
+  .use(sendAuthLink)
 
 app.listen(3333, () =>
   console.log('🚀 HTTP server running: http://localhost:3333'),
