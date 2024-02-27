97c988b4dfa6d2dc7a510dfb435e5a4a8e1be2a3
Kaue Alves <kaue_alves00@yahoo.com>
2024-02-27 14:48:46 -0300

    feat: Route - authenticate from link

--- src/http/server.ts
+++ src/http/server.ts
@@ -1,24 +1,13 @@
-import { Elysia, t } from 'elysia'
+import { Elysia } from 'elysia'
 
 import { RegisterRestaurant } from './routes/register-restaurant'
 import { sendAuthLink } from './routes/send-auth-link'
-import jwt from '@elysiajs/jwt'
-import { env } from '../env'
-import cookie from '@elysiajs/cookie'
+import { authenticateFromLink } from './routes/authenticate-from-link'
 
 const app = new Elysia()
-  .use(
-    jwt({
-      secret: env.JWT_SECRET,
-      schema: t.Object({
-        sub: t.String(),
-        restaurantId: t.Optional(t.String()),
-      }),
-    }),
-  )
-  .use(cookie())
   .use(RegisterRestaurant)
   .use(sendAuthLink)
+  .use(authenticateFromLink)
 
 app.listen(3333, () =>
   console.log('🚀 HTTP server running: http://localhost:3333'),
