a61abc19a885a933d99535f0294fa2d1b25a4606
Kaue Alves <kaue_alves00@yahoo.com>
2024-02-27 16:14:08 -0300

    feat: Route - Get profile

--- src/http/server.ts
+++ src/http/server.ts
@@ -3,11 +3,13 @@ import { Elysia } from 'elysia'
 import { RegisterRestaurant } from './routes/register-restaurant'
 import { sendAuthLink } from './routes/send-auth-link'
 import { authenticateFromLink } from './routes/authenticate-from-link'
+import { getProfile } from './routes/get-profile'
 
 const app = new Elysia()
   .use(RegisterRestaurant)
   .use(sendAuthLink)
   .use(authenticateFromLink)
+  .use(getProfile)
 
 app.listen(3333, () =>
   console.log('🚀 HTTP server running: http://localhost:3333'),
