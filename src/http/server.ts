a4048cf9ada8e3a6cc719bc04988b0016986974f
Kaue Alves <kaue_alves00@yahoo.com>
2024-02-27 12:37:45 -0300

    feat: Route - send auth link

--- src/http/server.ts
+++ src/http/server.ts
@@ -1,41 +1,9 @@
-import { Elysia, t } from 'elysia'
+import { Elysia } from 'elysia'
 
-import { db } from '../db/connection'
-import { restaurants, users } from '../db/schema'
+import { RegisterRestaurant } from './routes/register-restaurant'
+import { sendAuthLink } from './routes/send-auth-link'
 
-const app = new Elysia().post(
-  '/restaurants',
-  async ({ body, set }) => {
-    const { restaurantName, managerName, email, phone } = body
-
-    const [manager] = await db
-      .insert(users)
-      .values({
-        name: managerName,
-        email,
-        phone,
-        role: 'manager',
-      })
-      .returning({
-        id: users.id,
-      })
-
-    await db.insert(restaurants).values({
-      name: restaurantName,
-      managerId: manager.id,
-    })
-
-    set.status = 204
-  },
-  {
-    body: t.Object({
-      restaurantName: t.String(),
-      managerName: t.String(),
-      email: t.String(),
-      phone: t.String({ format: 'email' }),
-    }),
-  },
-)
+const app = new Elysia().use(RegisterRestaurant).use(sendAuthLink)
 
 app.listen(3333, () =>
   console.log('🚀 HTTP server running: http://localhost:3333'),
